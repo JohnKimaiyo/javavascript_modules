385769d81536eef6bfe094123a063caefb8c22fe
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-18 01:43:57 -0800

    export module

---
+++ project 1 - Copy (8)/app.js
@@ -0,0 +1,15 @@
+export default class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+export function printName(user) {
+    console.log(`users name is ${user.age}`)
+}
+
+export function printAge(user) {
+    console.log(`users is ${user.age} years old`)
+}
+
